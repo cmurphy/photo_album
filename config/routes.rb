dfc70d28482ab2d752b7e1d8f7c5329ebdd24700
Colleen Murphy <cmurphy@cat.pdx.edu>
2014-11-16 09:56:52 -0800

    Make photos index home

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  get 'photos/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index'
+  root 'photos#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
