6eb47f06319b2b21eb923abc681ae36301dfb589
Colleen Murphy <cmurphy@cat.pdx.edu>
2014-11-16 09:58:00 -0800

    Add next and previous navigation

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,4 +2,20 @@ class Photo < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   validates :name, presence: true
   validates :url,  presence: true
+
+  def next
+    photo = Photo.find_by_id(id + 1)
+    if ! photo
+      photo = Photo.first
+    end
+    photo
+  end
+
+  def prev
+    photo = Photo.find_by_id(id - 1)
+    if ! photo
+      photo = Photo.last
+    end
+    photo
+  end
 end
